85a7d13bfa37251aebe197223827c0a52e8ac64c
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-07-27 15:34:00 -0500

    added char

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.print("this is the second print");
+
+        char myCharacter = 'c';
     }
 }
