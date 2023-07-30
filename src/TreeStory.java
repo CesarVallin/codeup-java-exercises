3f5486fd3414e4f6b6e35a66afa81b5fa23ddcf3
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-07-30 16:41:27 -0500

    added loop and conditional statement to TreeStory IO

--- src/TreeStory.java
+++ src/TreeStory.java
@@ -7,42 +7,49 @@ public class TreeStory {
         System.out.print("What is your age?  ");
         String ageAsString = scanner.nextLine();
         // Parse integer... input String to int
-        int age = Integer.parseInt(ageAsString); // Integer's class static method parseInt, to generate a primitive int
+        int ageVariable = Integer.parseInt(ageAsString); // Integer's class static method parseInt, to generate a primitive int
         int ageLimit = 18;
-        if (age < ageLimit) {
+        if (ageVariable < ageLimit) {
             System.out.printf("You must be %s or older to run this program", ageLimit);
             System.exit(0);
         }
 
         // Input name...
         System.out.print("What is your name?  ");
-        String name = scanner.nextLine();
+        String nameVariable = scanner.nextLine();
 //        System.out.println("You entered: " + name);
         // Input adjective...
         System.out.print("Enter an adjective:  ");
-        String adjective = scanner.nextLine();
-        // Input noun...
-        System.out.print("Enter a noun:  ");
-        String noun = scanner.nextLine();
-        if (noun.equalsIgnoreCase("dork")
-            || noun.equalsIgnoreCase("jerk")) { // remember, because it is a String class, the noun variable has methods!!!!
-            System.out.print("That language is not allowed potty mouth! Exiting program \n\n");
-            System.exit(0);
-        }
+        String adjectiveVariable = scanner.nextLine();
+
+        // Variable declaration outside of loop scope...
+        String nounVariable;
+        boolean isInvalidWordVariable;
+        do {
+            System.out.print("Enter a noun:  ");
+            nounVariable = scanner.nextLine();
+
+            isInvalidWordVariable = nounVariable.equalsIgnoreCase("dork") || nounVariable.equalsIgnoreCase("jerk");
+
+            if (isInvalidWordVariable) {
+               System.out.printf("That language is not allowed potty mouth! Try again %n%n");
+            }
+        } while (isInvalidWordVariable);
+
         // Input adverb...
         System.out.print("Enter an adverb:  ");
-        String adverb = scanner.nextLine();
+        String adverbVariable = scanner.nextLine();
         // Input verb...
         System.out.print("Enter a verb:  ");
-        String verb = scanner.nextLine();
+        String verbVariable = scanner.nextLine();
 
         // OUTPUT Story... with format specifiers:
-        System.out.printf("%s is a %s %s.", name, adjective, noun);
-        System.out.printf(" He is always %s %s.\n", adverb, verb);
+        System.out.printf("%s is a %s %s.", nameVariable, adjectiveVariable, nounVariable);
+        System.out.printf(" He is always %s %s. %n%n", adverbVariable, verbVariable);
 
         // OUTPUT Story... with concatenation:
-        System.out.println(name + " is a " + adjective + " " + noun);
-        System.out.println("He is always " + adverb + " " + verb);
+        System.out.println(nameVariable + " is a " + adjectiveVariable + " " + nounVariable);
+        System.out.println("He is always " + adverbVariable + " " + verbVariable);
 
     }
 }
