f8d97c6aa0ec5d7826a66203857c2b25b7086ab2
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-07-31 13:32:58 -0500

    working on ControlFlowExercises

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        // ----------------------------------------------------------------------------------------
+//        // While loop:
+//        int i = 5;
+//        while(i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+        // ----------------------------------------------------------------------------------------
+        // Do while loop:
+//        // by 2s...
+//        int i = 0;
+//        do {
+//            if (i % 2 == 0) {
+//                System.out.println(i);
+//            }
+//            i++;
+//        } while (i <= 100);
+        // by 5s
+//        int i = 100;
+//        do {
+//            if (i % 5 == 0) {
+//                System.out.println(i);
+//            }
+//            i--;
+//        } while (i >= 5);
+//        // Squared
+//        long i = 2L;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i < 1000000);
+        // ----------------------------------------------------------------------------------------
+        // For loops:
+
+
+
+
+
+
+
+
+
+
+    }
+}
