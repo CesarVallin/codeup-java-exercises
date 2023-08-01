7f7675c56b83cb28813cb5ae1ef4b94411ab345a
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-07-31 19:49:46 -0500

    refactored ControlFlow and ConsoleExercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -71,92 +71,62 @@ public class ControlFlowExercises {
 //
 //        }
         // ----------------------------------------------------------------------------------------
-//        // Table of powers...
-//        System.out.print("Would you like to enter an integer? [y/N]  ");
-//        String userResponse = scanner.nextLine();
-//        boolean responseYes = userResponse.equalsIgnoreCase("y");
-//        if(responseYes) {
-//            System.out.println("Enter an integer:  ");
-//            int userInteger = scanner.nextInt();
-//            for (int i = 1; i <= userInteger; i++) {
-//                if (i == 1) {
-//                    System.out.println("number | squared | cubed");
-//                    System.out.println("------ | ------- | -----");
-//                    System.out.format( "%-7d| %-8d| %-5d", i, i, i);
-//                }
-//                if (i > 1) {
-//                    System.out.format("%n%-7d| %-8d| %-5d", i, (i * i), (i * i * i));
-//                }
-//            }
-//        } else {
-//            System.out.println("Maybe next time!");
-//            System.exit(0);
-//        }
+        // Table of powers...
+
+            boolean noContinue = true;
+            do {
+                System.out.println("Enter an integer:  ");
+                int userInteger = scanner.nextInt();
+                for (int i = 1; i <= userInteger; i++) {
+                    if (i == 1) {
+                        System.out.println("number | squared | cubed");
+                        System.out.println("------ | ------- | -----");
+                        System.out.format( "%-7d| %-8d| %-5d", i, i, i);
+                    }
+                    if (i > 1) {
+                        System.out.format("%n%-7d| %-8d| %-5d", i, (i * i), (i * i * i));
+                    }
+                }
+                System.out.format("%nWould you like to continue? [y/N]  ");
+                String userResponse = scanner.next();
+                if (userResponse.equalsIgnoreCase("y")) {
+                    noContinue = false;
+                }
+            } while(noContinue);
         // ----------------------------------------------------------------------------------------
         // Number Grade into letters...
 
-        boolean wackyResponse = false;
-
+        boolean repeat = true;
         do {
             System.out.print("Enter your numerical grade:  ");
             int grade = scanner.nextInt();
-            if (grade > 100 || grade < 0) {
-                wackyResponse = true;
-            }
 
+            String responseGrade;
             if (grade >= 99 && grade <= 100) {
-                System.out.format("Your grade: %d, is an A+", grade);
-                break;
+                responseGrade = "A+";
+                System.out.format("%d = %s", grade, responseGrade);
             } else if (grade >= 88 && grade < 99) {
-                System.out.format("Your grade: %d, is an A", grade);
-                break;
+                responseGrade = "A";
+                System.out.format("%d = %s", grade, responseGrade);
             } else if (grade >= 80 && grade < 88) {
-                System.out.format("Your grade: %d, is a B", grade);
-                break;
+                responseGrade = "B";
+                System.out.format("%d = %s", grade, responseGrade);
             } else if (grade >= 67 && grade < 80) {
-                System.out.format("Your grade: %d, is a C", grade);
-                break;
+                responseGrade = "C";
+                System.out.format("%d = %s", grade, responseGrade);
             } else if (grade >= 60 && grade < 67) {
-                System.out.format("Your grade: %d, is a D", grade);
-                break;
+                responseGrade = "D";
+                System.out.format("%d = %s", grade, responseGrade);
             } else if (grade > 0 && grade < 60) {
-                System.out.format("Your grade: %d, is an F", grade);
-                break;
-            } else {
-                System.out.format("Your response is not allowed, loop again %n");
+                responseGrade = "F";
+                System.out.format("%d = %s", grade, responseGrade);
             }
-        } while (wackyResponse);
-
-
-//        System.out.print("Enter your numerical grade:  ");
-//        int grade = scanner.nextInt();
-//        if (grade > 100 || grade < 0) {
-//            wackyResponse = true;
-//        }
-//
-//        if (grade >= 99) {
-//            System.out.format("Your grade: %d, is an A+", grade);
-//        } else if (grade >= 88 && grade < 99) {
-//            System.out.format("Your grade: %d, is an A", grade);
-//        } else if (grade >= 80 && grade < 88) {
-//            System.out.format("Your grade: %d, is a B", grade);
-//        } else if (grade >= 67 && grade < 80) {
-//            System.out.format("Your grade: %d, is a C", grade);
-//        } else if (grade >= 60 && grade < 67) {
-//            System.out.format("Your grade: %d, is a D", grade);
-//        } else if (grade >= 0 && grade < 60) {
-//            System.out.format("Your grade: %d, is an F", grade);
-//        } else {
-//            System.out.format("Your response is not allowed, loop again");
-//        }
-
-
-
-
-
-
-
-
+            System.out.format("%nWould you like to Continue? [y/N]?  ");
+            String userResponse = scanner.next();
+            if(userResponse.equalsIgnoreCase("y")) {
+                repeat = false;
+            }
+        } while (repeat);
 
 
 
