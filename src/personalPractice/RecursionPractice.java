0adf1b9d2e1944e5a24b390627e9689fec40f2c4
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-08-01 16:55:13 -0500

    working on recursion

--- src/personalPractice/RecursionPractice.java
+++ src/personalPractice/RecursionPractice.java
@@ -9,9 +9,19 @@ public class RecursionPractice {
         System.out.println(n);
         count(n -1);
     }
+    public static void countTimes(int n) {
+        if (n > 1000) {
+            System.out.println("All done!");
+            return;
+        }
+        System.out.println(n);
+        countTimes(n * 2); // Just think about `n` having some sort of memory within the program.
+    }
+
 
     public static void main (String[] args) {
-        count(5);
+//        count(5);
+        countTimes(2);
 
     }
 }
