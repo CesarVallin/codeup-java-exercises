b011b810e01af881915cadfdf024a32011c1726a
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-08-04 17:23:12 -0500

    Done with Input class including bonus, cleaning up code

--- src/util/Input.java
+++ src/util/Input.java
@@ -8,7 +8,7 @@ public class Input {
     private Scanner scanner;
     // ---------------------------------------------------------------------------------------------------------
     // Constructor... (creating a new instance of this object)
-    public Input(){
+    public Input() {
         this.scanner = new Scanner(System.in);
     }
     // ---------------------------------------------------------------------------------------------------------
@@ -17,15 +17,25 @@ public class Input {
         return this.scanner.nextLine();
     }
     public String getString(String prompt){
-        return prompt + " " + this.scanner.nextLine();
+        System.out.println(prompt);
+        return getString();
     }
+    // -----------------------------------------
     public boolean yesNo() {
         boolean yes = this.scanner.nextLine().toLowerCase().startsWith("y");
         return yes;
     }
-
+    public boolean yesNo(String prompt){
+        System.out.println(prompt);
+        return yesNo();
+    }
+    // -----------------------------------------
     public int getInt() {
         return this.scanner.nextInt();
+    }
+    public int getInt(String prompt) {
+        System.out.println(prompt);
+        return getInt();
     }
     public int getInt(int min, int max) {
         int userResponse;
@@ -35,14 +45,37 @@ public class Input {
         } while(userResponse < min || userResponse > max);
         return userResponse;
     }
+    public int getInt(String prompt, int min, int max) {
+        int userResponse;
+        do {
+            System.out.println(prompt);
+            userResponse = getInt();
+        } while (userResponse < min || userResponse > max);
+        return userResponse;
+    }
+    // -----------------------------------------
 
     public double getDouble() {
         return this.scanner.nextDouble();
     }
+    public double getDouble(String prompt) {
+        System.out.println(prompt);
+        return getDouble();
+    }
+
     public double getDouble(double min, double max) {
         double userResponse;
         do {
             System.out.println("Enter a decimal number between " + min + " and " + max);
+            userResponse = getDouble();
+        } while(userResponse < min || userResponse > max);
+        return userResponse;
+    }
+
+    public double getDouble (String prompt, double min, double max) {
+        double userResponse;
+        do {
+            System.out.println(prompt);
             userResponse = getDouble();
         } while(userResponse < min || userResponse > max);
         return userResponse;
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -8,37 +8,56 @@ public class InputTest {
         Input input = new Input();
 
         // ---------------------------------------------------------------------------------------------------------
-        // Testing out getString() method...
-        System.out.format("Enter something here:%n");
+//        // Testing out getString() method...
+//        System.out.format("Enter something here:%n");
 //        String userResponse = input.getString();
 //        System.out.println(userResponse);
-
-        // Optional string parameter named prompt
-        String userResponseWithPrompt = input.getString("You entered");
-        System.out.println(userResponseWithPrompt);
+//        // Optional string parameter prompt
+//        String userResponseWithPrompt = input.getString("Please enter something here...");
+//        System.out.println(userResponseWithPrompt);
         // ---------------------------------------------------------------------------------------------------------
 //        // Testing out yesNo() method...
 //        System.out.format("Do you want to continue? [y/N]%n");
 //        boolean continueResponse = input.yesNo();
 //        System.out.println(continueResponse);
+//        // Optional string parameter prompt
+//        boolean continuePromptIncluded = input.yesNo("Please answer, Do you want to continue? [y/N]");
+//        System.out.println(continuePromptIncluded);
         // ---------------------------------------------------------------------------------------------------------
 //        // Testing out getInt() method...
-//        System.out.format("Enter an integer:%n");
+//        System.out.format("Enter an number:%n");
 //        int integerResponse = input.getInt();
 //        System.out.println(integerResponse);
+//        // Optional int parameter prompt
+//        int intPromptIncluded = input.getInt("Please enter a number ...");
+//        System.out.println(intPromptIncluded);
         // ---------------------------------------------------------------------------------------------------------
 //        // Testing out minMax getInt() method...
 //        int minMaxI = input.getInt(20, 40);
 //        System.out.println(minMaxI);
+//        // Optional int minMax parameter prompt
+//        int minMaxII = input.getInt("Please enter number between 1 and 10", 1, 10);
+//        System.out.println(minMaxII);
         // ---------------------------------------------------------------------------------------------------------
-//        // Testing out getDouble() method...
+
+        // getDouble() method...
+
+        // Testing out getDouble() method...
 //        System.out.format("Enter an decimal number:%n");
 //        double doubleResponse = input.getDouble();
 //        System.out.println(doubleResponse);
+//        // Optional getDouble() method with parameter prompt...
+//        double doublePrompt = input.getDouble("Please enter a decimal number");
+//        System.out.println(doublePrompt);
         // ---------------------------------------------------------------------------------------------------------
-//        // Testing out minMax getDouble() method...
-//        double minMaxD = input.getDouble(20.0, 40.0);
-//        System.out.println(minMaxD);
+
+        // getDouble() method, minMax ....
+
+        // Testing out minMax getDouble() method...
+        double minMaxD = input.getDouble(20.0, 40.0);
+        System.out.println(minMaxD);
+        double minMaxDprompt = input.getDouble("Please enter a decimal number between 20.00 and 40.00", 20.00, 40.00);
+        System.out.println(minMaxDprompt);
 
 
 
