09d2f66ba0deb414d2a284a20e5bd8ebf72a4f9a
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-08-04 18:09:20 -0500

    working on Circles class

---
+++ src/shapes/Circle.java
@@ -0,0 +1,23 @@
+package shapes;
+
+import util.Input;
+public class Circle {
+
+    // ---------------------------------------------------------------------------------------------------------
+    // Instance properties (fields)...
+    private double radius;
+    // ---------------------------------------------------------------------------------------------------------
+    // Constructors...
+    // non-arg constructor
+    public Circle() {
+        this.radius = 1;
+    }
+    // arg constructor
+    public Circle(double newRadius) {
+        this.radius = newRadius;
+    }
+    // ---------------------------------------------------------------------------------------------------------
+    // Getters & Setters
+
+
+}
