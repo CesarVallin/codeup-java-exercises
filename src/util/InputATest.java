3f91b271e475d89dd4d4f0927536466201fae0b7
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-08-07 10:46:34 -0500

    circles walk thru

---
+++ src/util/InputATest.java
@@ -0,0 +1,25 @@
+package util;
+
+public class InputATest {
+    public static void main(String[] args) {
+        InputA inputA = new InputA();
+
+//        System.out.println("Please enter a string");
+//        System.out.println(input.getString());
+
+//        System.out.println("Do you want to continue? Y|N");
+//        System.out.println(input.yesNo());
+
+//        System.out.println("enter a number");
+//        System.out.println(input.getInt());
+
+//        System.out.println("enter a number between 1 and 10");
+//        System.out.println(input.getInt(1, 10));
+
+//        System.out.println("enter a number between 1.0 and 10.0");
+//        System.out.println(input.getDouble(1.0, 10.0));
+
+        System.out.println(inputA.getString("Please enter your name"));
+
+    }
+}
