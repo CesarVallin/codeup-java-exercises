df7bd3a516e282431ce4441522af87e3758074a1
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-08-08 12:38:37 -0500

    Changes on pearsonInheritance

--- src/pearsonsInheritance/TestCircleGORectangleGO.java
+++ src/pearsonsInheritance/TestCircleGORectangleGO.java
@@ -1,12 +1,17 @@
 package pearsonsInheritance;
 
 public class TestCircleGORectangleGO {
+
+    public static void circleInfoPrompt(CircleGO varName) {
+        System.out.println("A circle " + varName.toStringG()); // from superClass
+        System.out.println("The color is: " + varName.getColor()); // from superClass
+        System.out.println("The radius is: " + varName.getRadius()); // subClass method
+        System.out.println("the area is: " + varName.getArea()); // subClass method
+        System.out.println("The diameter is: " + varName.getDiameter()); // subClass method
+    }
+
     public static void main(String[] args) {
         CircleGO circle1 = new CircleGO("green", true, 3.45);
-        System.out.println("A circle " + circle1.toStringG()); // from superClass
-        System.out.println("The color is: " + circle1.getColor()); // from superClass
-        System.out.println("The radius is: " + circle1.getRadius()); // subClass method
-        System.out.println("the area is: " + circle1.getArea()); // subClass method
-        System.out.println("The diameter is: " + circle1.getDiameter()); // subClass method
+        circleInfoPrompt(circle1);
     }
 }
