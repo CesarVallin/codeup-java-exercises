721720e8638f0a11727a81b4839a35645456a4ed
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-08-08 17:52:55 -0500

    Student and StudentTest completed

---
+++ src/grades/Student.java
@@ -0,0 +1,44 @@
+package grades;
+
+import java.util.ArrayList;
+import util.Input;
+
+
+public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+
+    /** Constructor */
+
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    /** Get name */
+    public String getName() {
+        return this.name;
+    }
+    /** Add grade */
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+    /** Get all grades */
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    /** Return the average of the grades */
+    public double getGradeAverage() {
+        double average = 0;
+        for (int i = 0; i < grades.size(); i++) {
+            double currentIndexedGrade = grades.get(i);
+            average += currentIndexedGrade;
+        }
+        return average / grades.size();
+    }
+
+
+
+
+}
---
+++ src/grades/StudentTest.java
@@ -0,0 +1,39 @@
+package grades;
+
+public class StudentTest {
+
+    public static void messageFormat(Student studentVar) {
+        if(studentVar.getGrades().isEmpty()) {
+            System.out.format("%s %s%n", studentVar.getName(), studentVar.getGrades());
+        } else {
+            System.out.format("%s %s %.2f%n", studentVar.getName(), studentVar.getGrades(), studentVar.getGradeAverage());
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Student cesar = new Student("Cesar");
+        messageFormat(cesar);
+        cesar.addGrade(89);
+        cesar.addGrade(78);
+        cesar.addGrade(93);
+        messageFormat(cesar);
+        Student luis = new Student("Luis");
+        messageFormat(luis);
+        luis.addGrade(93);
+        luis.addGrade(94);
+        luis.addGrade(77);
+        messageFormat(luis);
+        Student sam = new Student("Sam");
+        messageFormat(sam);
+        sam.addGrade(93);
+        sam.addGrade(94);
+        sam.addGrade(77);
+        messageFormat(sam);
+
+
+
+
+    }
+}
