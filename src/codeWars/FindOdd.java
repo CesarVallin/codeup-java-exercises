239989de08e60b1284411a632f1f54991a59a542
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-08-22 22:03:46 -0500

    completed FindOdd in Codewars

--- src/codeWars/FindOdd.java
+++ src/codeWars/FindOdd.java
@@ -2,28 +2,37 @@ package codeWars;
 
 public class FindOdd {
     public static void main(String[] args) {
-//        System.out.println(findIt(numbers));
+        System.out.println(findIt(numbers));
         // expected answer is 0;
-//        System.out.println(findIt(numbers1));
         System.out.println(findIt(numbers1));
+        System.out.println(findIt(numbers2));
     }
 
 
-//    private static int[] numbers = {0, 1, 0, 1, 0};
+    private static int[] numbers = {0, 1, 0, 1, 0};
     private static int[] numbers1 = {1,2,2,3,3,3,4,3,3,3,2,2,1};
-//    private static int [] numbers2 = {1, 1, 2};
+    private static int [] numbers2 = {1, 1, 2};
 
     public static int findIt(int[] a) {
 
-        int
+        if (a.length == 1) {
+            return a[0];
+        }
+        int counter = 0;
         int index = 0;
         do {
             for(int i = 0; i < a.length; i ++) {
-                if (a[index] == a[i])
+                if (a[index] == a[i]) {
+                   counter += 1;
+                }
             }
-        }while ()
+            if (counter % 2 != 0) {
+                break;
+            }
+            index ++;
+        }while (index < a.length);
+        return a[index];
 
-        return 0; // odd Number
     }
 
 }
