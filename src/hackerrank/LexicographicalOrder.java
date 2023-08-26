94a29780fd04b5214fec314115fb0b7b589ee048
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-08-26 11:04:22 -0500

    prgress in HackerRank LexicographicalOrder

--- src/hackerrank/LexicographicalOrder.java
+++ src/hackerrank/LexicographicalOrder.java
@@ -8,16 +8,26 @@ public class LexicographicalOrder {
     private static String test1 = "welcometojava";
 
     public static String getSmallestAndLargest(String s, int k) {
+        String currentSubstring = s.substring(0, k);
         String smallest = "";
         String largest = "";
 
         // Complete the function
         // 'smallest' must be the lexicographically smallest substring of length 'k'
         // 'largest' must be the lexicographically largest substring of length 'k'
+        for (int i = 0; i <= s.length() - k; i++) {
+            for (int j = i + 1; j < k; j++) {
+
+            }
+        }
 
         return smallest + "\n" + largest;
     }
 
+    /**
+     * Test trial...
+     * Use casting: To get the ASCII value of a character, we can simply cast our char as an int.
+     * */
     public static void testAscii(String s) {
         for(int i = 0; i < s.length(); i++) {
             int asciiValue = s.charAt(i);
