42027696d7313fa3940c40d6576cd21044ae696f
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-08-25 22:02:53 -0500

    created a nested for loop for findIt method

--- src/codeWars/FindOdd.java
+++ src/codeWars/FindOdd.java
@@ -3,9 +3,13 @@ package codeWars;
 public class FindOdd {
     public static void main(String[] args) {
         System.out.println(findIt(numbers));
-        // expected answer is 0;
         System.out.println(findIt(numbers1));
         System.out.println(findIt(numbers2));
+
+        System.out.println("\nTrying it with two nested for loops\n");
+        System.out.println(findIt2(numbers));
+        System.out.println(findIt2(numbers1));
+        System.out.println(findIt2(numbers2));
     }
 
 
@@ -32,6 +36,33 @@ public class FindOdd {
         }while (index < a.length);
         return a[index];
 
+    }
+
+    /**
+     * Same output as above, however this one was done with two nested for loops
+     * The first loop, index i, stays at the current index, while index j iterates through all the
+     * indices of a.length, then i would move from index 0 to index 1...
+     * At this point, j loop would start again, and so forth until i reaches its condition: a.length
+     * */
+    public static int findIt2(int[] a) {
+        if (a.length == 1) {
+            return a[0];
+        }
+        int counter = 0;
+        int indexValue = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (a[i] == a[j]) {
+                    counter ++;
+                }
+            }
+            if (counter % 2 != 0) {
+                indexValue = a[i];
+                break;
+            }
+
+        }
+        return indexValue;
     }
 
 }
