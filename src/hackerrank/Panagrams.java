f2122601458c2440235c00479b4d357d30866eda
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-01 00:22:15 -0500

    completed pangrams hackerrank java

--- src/hackerrank/Panagrams.java
+++ src/hackerrank/Panagrams.java
@@ -4,16 +4,31 @@ import java.util.Arrays;
 
 public class Panagrams {
     public static void main(String[] args) {
-
+        System.out.println(pangrams("We promptly judged antique ivory buckles for the next prize"));
     }
 
     public static String pangrams(String s) {
         // Write your code here
-        Boolean[] alphabet = new Boolean[26]; // 26 letters in alphabet
-        Arrays.fill(alphabet, false); // Filling every index of the Boolean[] alphabet array with false
+        final int ALPHABET_COUNT = 26;
+
+        Boolean[] alphabetArray = new Boolean[ALPHABET_COUNT]; // 26 letters in alphabet
+        Arrays.fill(alphabetArray, false); // Filling every index of the Boolean[] alphabet array with false
+
         int alphabetIndex = 0;
-        s = s.toLowerCase();
 
+        s = s.toUpperCase();
 
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+                alphabetIndex = s.charAt(i) - 'A';
+                alphabetArray[alphabetIndex] = true;
+            }
+        }
+        for (boolean trueElement : alphabetArray) {
+            if(!trueElement) {
+                return "not pangram";
+            }
+        }
+        return "pangram";
     }
 }
