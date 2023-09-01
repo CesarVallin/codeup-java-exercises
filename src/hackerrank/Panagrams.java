7c0a05f0ed986dd4f8319788da3e7d40e0117663
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-08-31 21:58:03 -0500

    working on panagrams hacker rank java

---
+++ src/hackerrank/Panagrams.java
@@ -0,0 +1,19 @@
+package hackerrank;
+
+import java.util.Arrays;
+
+public class Panagrams {
+    public static void main(String[] args) {
+
+    }
+
+    public static String pangrams(String s) {
+        // Write your code here
+        Boolean[] alphabet = new Boolean[26]; // 26 letters in alphabet
+        Arrays.fill(alphabet, false); // Filling every index of the Boolean[] alphabet array with false
+        int alphabetIndex = 0;
+        s = s.toLowerCase();
+
+
+    }
+}
