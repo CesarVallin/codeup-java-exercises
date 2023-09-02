2d3130d14df9b824eea4104bc36fe4e4a80d7c5e
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-01 22:38:55 -0500

    DateAndTime hackkerrank

---
+++ src/hackerrank/DateAndTime.java
@@ -0,0 +1,17 @@
+package hackerrank;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+public class DateAndTime {
+    public static void main(String[] args) {
+
+        System.out.println(findDay(8, 14, 2017));
+
+    }
+    public static String findDay(int month, int day, int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, day);
+        return calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, new Locale("en", "US")).toUpperCase();
+    }
+}
