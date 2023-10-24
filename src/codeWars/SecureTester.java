98603324516e20cc647fae91e7b36ccb387fd462
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-10-23 19:53:26 -0500

    completed SecureTester challange from Codewars

---
+++ src/codeWars/SecureTester.java
@@ -0,0 +1,33 @@
+package codeWars;
+
+public class SecureTester {
+    public static boolean alphanumeric(String s){
+
+        /** Not needed to check for empty string, but good practice*/
+        if(s.isEmpty()) {
+            return false;
+        }
+
+//        for (int i = 0; i < s.length(); i++) {
+//            if(!Character.isLetter(s.charAt(i)) && !Character.isDigit(s.charAt(i))) {
+//                return false;
+//            }
+//        }
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//            if (!Character.isLetterOrDigit(c)) {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+        return s.matches("[a-zA-Z0-9]+");
+    }
+
+}
+
+/**
+ * the Character.isLetterOrDigit(c) method may not handle non-ASCII characters like "ï" as expected. To accurately handle these cases, you can use regular expressions with the matches method to ensure that the string only contains alphanumeric characters.
+ *
+ * */
--- src/hackerrank/Test.java
+++ src/hackerrank/Test.java
@@ -1,13 +1,14 @@
 package hackerrank;
 
-import codeWars.CharProblem;
+import codeWars.SecureTester;
 
 public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println(CharProblem.howOld("5 years old"));
-
+        System.out.println(SecureTester.alphanumeric("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"));
+        System.out.println(SecureTester.alphanumeric("with_underscore"));
+        System.out.println(SecureTester.alphanumeric("naïve"));
     }
 
 }
