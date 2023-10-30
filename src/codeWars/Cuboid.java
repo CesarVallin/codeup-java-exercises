51ffbb9b165f5860b7a83ec6f6d290d960158e70
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-10-30 18:47:57 -0500

    Cuboid challange

---
+++ src/codeWars/Cuboid.java
@@ -0,0 +1,9 @@
+package codeWars;
+
+public class Cuboid {
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        // Your code here
+        double volume = length * width * height;
+        return volume;
+    }
+}
