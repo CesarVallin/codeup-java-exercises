b50556473bb997919a8107995c050c82e1da857b
Cesar Vallin <cesarvallin.cv@gmail.com>
2024-01-02 20:46:20 -0600

    NumberString

---
+++ src/codeWars/NumberString.java
@@ -0,0 +1,13 @@
+package codeWars;
+
+public class NumberString {
+
+    public static String numberToString(int num) {
+
+        String numberToString = String.valueOf(num);
+
+        return numberToString;
+
+    }
+
+}
