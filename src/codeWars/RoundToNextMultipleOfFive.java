4299106a26c5c78a6912b3540812e87d21595936
Cesar Vallin <cesarvallin.cv@gmail.com>
2024-01-03 19:51:10 -0600

    notes and extra learning on challange

--- src/codeWars/RoundToNextMultipleOfFive.java
+++ src/codeWars/RoundToNextMultipleOfFive.java
@@ -8,13 +8,31 @@ public class RoundToNextMultipleOfFive {
             return number;
         }
 
-
+/**
+ * This code rounds to the nearest integer that is divisible by 5.
+ * */
 //        int roundUp5 = (int) Math.round( (float) number / 5) * 5;
 //        return roundUp5;
 
+        /**
+         *  This code rounds up to the next integer that is divisible by 5.
+         *
+         *  Math.ceil always rounds up to the next integer.
+         * */
         int roundUp5 = (int) (Math.ceil( (float) number / 5) * 5);
         return roundUp5;
 
     }
 
 }
+
+
+/**
+ * Check out this other solution:
+ *
+ * while (number % 5 != 0) {
+ *       number++;
+ *     }
+ *     return number;
+ *
+ * */
