5ab9828295779df85274231206425664c83aba49
Cesar Vallin <cesarvallin.cv@gmail.com>
2024-01-23 17:16:22 -0600

    further understanding of internal loop

--- src/codeWars/Mumbling.java
+++ src/codeWars/Mumbling.java
@@ -14,6 +14,12 @@ public class Mumbling {
             landingBase.append(Character.toUpperCase(c));
 
             // Append lowercase character i times
+            /** The loop variable `j` is initialized to 0
+             * The loop condition `j < i` ensures that the loop will execute as long as `j` is less than the current value of `i`
+             *
+             * This double loop (internal loop) is utilized instead of the `repeat` method.
+             * In each iteration of the loop, the append method is used to add a lowercase version of the character c to the landingBase
+             * */
             for (int j = 0; j < i; j++) {
                 landingBase.append(Character.toLowerCase(c));
             }
