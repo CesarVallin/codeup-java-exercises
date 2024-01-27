f72184088e379060ef259047bdf3d52d7b5da259
Cesar Vallin <cesarvallin.cv@gmail.com>
2024-01-26 19:41:11 -0600

    TwoSum comparison of results

--- src/codeWars/TwoSum.java
+++ src/codeWars/TwoSum.java
@@ -18,3 +18,22 @@ public class TwoSum {
         return result;
     }
 }
+
+/**
+ * Check out how you tested it on the Test class:
+ *
+ *         System.out.println(Arrays.toString(TwoSum.twoSum(numbers, 4)));
+ * */
+
+
+/**
+ * Check out this solution:
+ *
+ * Another approach:
+ *      - Start by declaring and initializing the array with two indices.
+ *      int[] result = new int[2];
+ *
+ *      - Nested inside the double loops, the code block within your conditional statement:
+ *      result[0] = i;
+ *      result[1] = j;
+ * */
